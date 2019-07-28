ce38b5a5642918627b53cdc770ad4b283b02df73
Niruel <npruppel@gmail.com>
2019-07-29 07:33:38 +0900

    nlther

---
+++ ReacApp/app/build/precache-manifest.dd50acdc2331ca7d993127dc681a0026.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb0c0dc3775012e6328aeb7c39d94c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e65463a318c6c4b00b9d",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "70f7f3fc0f0c4f3abd46",
+    "url": "/static/css/main.e8d9d8e9.chunk.css"
+  },
+  {
+    "revision": "e65463a318c6c4b00b9d",
+    "url": "/static/js/2.d4b5516f.chunk.js"
+  },
+  {
+    "revision": "70f7f3fc0f0c4f3abd46",
+    "url": "/static/js/main.36a50a24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fef16dc961586916068e19e7ea66ff82",
+    "url": "/static/media/Logo_svg.fef16dc9.svg"
+  },
+  {
+    "revision": "d13219964ba27a8d6063591c140cd3f8",
+    "url": "/static/media/TormentLogo.d1321996.jpg"
+  },
+  {
+    "revision": "4f47010a196140e692ad01cdd11e4b9f",
+    "url": "/static/media/Torment_logo.4f47010a.jpg"
+  },
+  {
+    "revision": "d284015180158fee2fdc18238f69c377",
+    "url": "/static/media/gamepad-game-controller-svgrepo-com.d2840151.svg"
+  },
+  {
+    "revision": "ea8bb7f4b65e7f8b2438d45463270d82",
+    "url": "/static/media/github.ea8bb7f4.svg"
+  },
+  {
+    "revision": "549aa90f8f16079d1c210e7bb554a661",
+    "url": "/static/media/hat-cultures-svgrepo-com.549aa90f.svg"
+  },
+  {
+    "revision": "8220d48085242e88ff8450680fdd8f7e",
+    "url": "/static/media/linkedin-logo.8220d480.svg"
+  },
+  {
+    "revision": "ccbc6bb0ae645df88707d6bada92a25e",
+    "url": "/static/media/tools-hand-drawn-outlines-of-configuration-interface-symbol-svgrepo-com.ccbc6bb0.svg"
+  },
+  {
+    "revision": "640c6ace05b450df91e9c0afc2b8131a",
+    "url": "/static/media/torii-gate-japan-svgrepo-com.640c6ace.svg"
+  },
+  {
+    "revision": "93a9fd6aa377d5fe5cb299c3927b8a15",
+    "url": "/static/media/twitter.93a9fd6a.svg"
+  },
+  {
+    "revision": "b3f61e3f352ec88fea972663fa79247b",
+    "url": "/static/media/youtube.b3f61e3f.svg"
+  }
+]);
--- ReacApp/app/build/service-worker.js
+++ ReacApp/app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slumberingdragonstudios/precache-manifest.f232ac1e3918966c068d3f79c9c0bd6a.js"
+  "/precache-manifest.dd50acdc2331ca7d993127dc681a0026.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slumberingdragonstudios/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
