de69c3654b81697a72da5f5ad877f31b2699773f
ニルエル <npruppel@gmail.com>
2022-10-11 17:35:32 +0900

    Updates

---
+++ precache-manifest.7692aa8ef47bd85700d2c3583c4cfc6a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f63612fab6fa10eaff9",
+    "url": "/static/css/main.4049bee7.chunk.css"
+  },
+  {
+    "revision": "8f63612fab6fa10eaff9",
+    "url": "/static/js/main.8f63612f.chunk.js"
+  },
+  {
+    "revision": "0db833691355cb9d284f",
+    "url": "/static/js/1.0db83369.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70c8e88e16ce315dba08bc9f04dadf5b",
+    "url": "/static/media/GamingChef.70c8e88e.png"
+  },
+  {
+    "revision": "c2c4f862c19b1774270b0fb05698e4f8",
+    "url": "/static/media/pg06nick_3880_BW.c2c4f862.jpg"
+  },
+  {
+    "revision": "8f2f4c71a850b23280e967eb7f7d7b8d",
+    "url": "/static/media/bitsummit-image-logo.8f2f4c71.png"
+  },
+  {
+    "revision": "c7b0fe66651ba6e2a38de74c5dad214f",
+    "url": "/static/media/segalogo.c7b0fe66.jpg"
+  },
+  {
+    "revision": "d1655e65ccda4b77e3c1944657455d33",
+    "url": "/static/media/replayingjplogo.d1655e65.png"
+  },
+  {
+    "revision": "d13219964ba27a8d6063591c140cd3f8",
+    "url": "/static/media/TormentLogo.d1321996.jpg"
+  },
+  {
+    "revision": "4f47010a196140e692ad01cdd11e4b9f",
+    "url": "/static/media/Torment_logo.4f47010a.jpg"
+  },
+  {
+    "revision": "964f6f749fa43b317bd77a35b9769076",
+    "url": "/static/media/fin.964f6f74.jpg"
+  },
+  {
+    "revision": "cc10776cfe577fdf749392540650cde2",
+    "url": "/static/media/devBuild1.cc10776c.jpg"
+  },
+  {
+    "revision": "44da419f1842939ca40509760e39dd0b",
+    "url": "/static/media/devBuild2.44da419f.jpg"
+  },
+  {
+    "revision": "d00deb359c89b6b86748892f30c93c9a",
+    "url": "/static/media/devBuild3.d00deb35.jpg"
+  },
+  {
+    "revision": "fef16dc961586916068e19e7ea66ff82",
+    "url": "/static/media/Logo_svg.fef16dc9.svg"
+  },
+  {
+    "revision": "640c6ace05b450df91e9c0afc2b8131a",
+    "url": "/static/media/torii-gate-japan-svgrepo-com.640c6ace.svg"
+  },
+  {
+    "revision": "549aa90f8f16079d1c210e7bb554a661",
+    "url": "/static/media/hat-cultures-svgrepo-com.549aa90f.svg"
+  },
+  {
+    "revision": "ccbc6bb0ae645df88707d6bada92a25e",
+    "url": "/static/media/tools-hand-drawn-outlines-of-configuration-interface-symbol-svgrepo-com.ccbc6bb0.svg"
+  },
+  {
+    "revision": "d284015180158fee2fdc18238f69c377",
+    "url": "/static/media/gamepad-game-controller-svgrepo-com.d2840151.svg"
+  },
+  {
+    "revision": "ea8bb7f4b65e7f8b2438d45463270d82",
+    "url": "/static/media/github.ea8bb7f4.svg"
+  },
+  {
+    "revision": "8220d48085242e88ff8450680fdd8f7e",
+    "url": "/static/media/linkedin-logo.8220d480.svg"
+  },
+  {
+    "revision": "93a9fd6aa377d5fe5cb299c3927b8a15",
+    "url": "/static/media/twitter.93a9fd6a.svg"
+  },
+  {
+    "revision": "b3f61e3f352ec88fea972663fa79247b",
+    "url": "/static/media/youtube.b3f61e3f.svg"
+  },
+  {
+    "revision": "d6e91ba5cdcb762bbbbf5ed475f39563",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38ba5146e5f9b440b49c1d6fc4912076.js"
+  "/precache-manifest.7692aa8ef47bd85700d2c3583c4cfc6a.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
